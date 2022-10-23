777698a75f15242c68b51a8c8afb75e6907da327
jasongbragg <jasongbragg@gmail.com>
2022-10-23 20:57:06 +1100

    sfs, nei

---
+++ R/psfs_diversity.r
@@ -0,0 +1,37 @@
+#' Calculates projected sfs, given a set of genotypes (gt),
+#' a value for down-projection (m), and a vector of weight 
+#' values (equal length to the number of individuals) 
+#' 
+#' @param gt  - genotype matrix (individuals, loci)
+#' @param w   - vector of weights
+#' @param m   - projection value
+#' @return psfs, a float value 
+#' @author Jason Bragg (jasongbragg@gmail.com)
+#' @export
+
+psfs_diversity <- function(gt, m, w=NULL) {
+
+   if (is.null(m)) {
+      m = 20
+   }
+
+   # estimate allele frequencies
+   if (is.null(w)) {
+      wgt = gt
+   } else {
+
+      if (nrow(gt) == length(w)) {
+         iw <- rep(1:nrow(gt),w)
+         wgt <- gt[iw,]
+      } else {
+         cat("   weights supplied: must have length equal to number of rows in gt \n")
+      }
+   }
+
+   projected_SFS <- project_SFS_from_genotypes(wgt, m)
+   fixedSFS <- projected_SFS[1] + projected_SFS[m+1]
+   ppSFS <- (sum(projected_SFS) - fixedSFS)/sum(projected_SFS)
+
+   return(ppSFS)
+}
+
